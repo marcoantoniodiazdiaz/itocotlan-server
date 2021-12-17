44e64f85e28258bb9ea2212f2258cce048eccfb5
Marco Antonio Diaz Diaz <kingmarkdc@gmail.com>
2021-12-17 11:47:11 -0600

    Update database.ts

--- database/database.ts
+++ database/database.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize({
     database: 'itocotlan',
     host: 'localhost',
-    password: 'pataPON240612',
+    password: 'keendc2000',
     username: 'root',
     dialect: 'mysql',
     logging: false,
