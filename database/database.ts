7a6c8b7c8ea744b717fb9d423a9e96fb797370ca
marcoantoniodiazdiaz <kingmarkdc@gmail.com>
2021-12-17 17:44:04 +0000

    Contrasenas

--- database/database.ts
+++ database/database.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize({
     database: 'itocotlan',
     host: 'localhost',
-    password: 'root',
+    password: 'pataPON240612',
     username: 'root',
     dialect: 'mysql',
     logging: false,
